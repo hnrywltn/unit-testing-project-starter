1f167e605d454f21a68592d235da2918a80829fd
henry walton <hnrywltn@gmail.com>
2021-04-15 16:31:53 -0700

    almost done

--- problems/phase-2.js
+++ problems/phase-2.js
@@ -9,46 +9,40 @@ const exampleArray2 = ['a', 'b', 'c']
 mirrorArray(exampleArray2) >> ['a', 'b', 'c', 'c', 'b', 'a']
 */
 const mirrorArray = (array) => {
-  let newArray = [];
-
-  for (let i = 0; i < array.length; i += 1) {
-    let el = array[i];
-    newArray.push(el);
-  }
-
-  for (let i = array.length - 1; i >= 0; i += 1) {
-    let el = array[i];
-    newArray.push(el);
-  }
-
-  return newArray;
+  let newArray = [...array];
+  return newArray.concat(array.reverse());
 };
+
+
 
 /*
 2.  hiddenCounter
 Write a function named `hiddenCounter()`. This function will do two things - first
-it will define a count variable, then it will return a function. 
+it will define a count variable, then it will return a function.
 When invoked the function returned by hiddenCounter will increment the counter by 1.
 */
 function hiddenCounter() {
-  let count = undefined;
-  return () => (count += 1);
+  let count = 0;
+  return () => count += 1;
 }
+
 /*
-3. myMap 
+3. myMap
 Write a function  named `myMap` that accepts an array
 and a callback as arguments. The function will return an array of new elements
-obtained by calling the callback on each element of the array, passing in the 
+obtained by calling the callback on each element of the array, passing in the
 element. Assign the below function to a variable using the const keyword.
 
 Do not use the built in Array#map - use Array#forEach for iteration.
 */
 const myMap = (array, cb) => {
   let mapped = [];
-
-  array.forEach((el) => mapped.push(el));
+  array.forEach((el) => {
+    mapped.push(cb(el))
+  });
   return mapped;
 };
+
 
 /*
 4. AvgValue
@@ -56,11 +50,8 @@ Write a function , `avgValue(array)` that takes in an
 array of numbers and returns the average number. Assign the below function to a
 variable using the const keyword.
 */
-const avgValue = (array) => {
-  let sum = array.reduce((el, sum = 0) => (sum += el));
+const avgValue = (array) => array.reduce((accum, el) => (accum += el)) / array.length;
 
-  let avg = sum / array.length - 1;
-  return avg;
-};
+// console.log(avgValue([1, 2, 3]))
 
 module.exports = { mirrorArray, hiddenCounter, myMap, avgValue };
--- test/phase-2-spec.js
+++ test/phase-2-spec.js
@@ -8,58 +8,47 @@ const { mirrorArray, hiddenCounter, myMap, avgValue } = require('../problems/pha
 //1. mirrorArray
 describe('mirrorArray()', function () {
   it('should return a single array where the first half is the orginal array and the second half is a mirror of the first half', function () {
-    //Arrange
-
-    //Act
-    
-    //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    let input = ['a', 'b', 'c'];
+    let expected = ['a', 'b', 'c', 'c', 'b', 'a'];
+    let actual = mirrorArray(input);
+    expect(actual).to.eql(expected);
   })
 })
 
 //2. hiddenCounter
 describe('hiddenCounter()', function () {
-  
+
   it('should return a function that will increment the counter when invoked', function () {
-    //Arrange
-
-    //Act
-
-    //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    let expected = 'function';
+    let actual = hiddenCounter();
+    let expected2 = 1;
+    let actual2 = actual();
+    expect(typeof actual).to.equal(expected);
+    expect(actual2).to.equal(expected2)
   })
 })
 
 //3. myMap
 describe('myMap', function () {
  it("should function like the built in Array#map", function () {
-    //Arrange
-   
-    //Act
-   
-    //Assert
-   expect.fail('Remove this expect.fail and replace it with your test');
+  let double = (num) => {
+    return num * 2
+  };
+  let expected = [2, 4, 6];
+  let actual = myMap([1, 2, 3], double);
+  expect(actual).to.eql(expected);
   });
 
   it("should not call the built in Array#map", function () {
-     //Arrange
-    
-    //Act
-
-    //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
+    let myMapSpy = chai.spy.on(Array.map)
   });
 })
 
 //4. avgValue
 describe('avgValue', function () {
   it('should return the average of an array of numbers', function () {
-     //Arrange
-    
-    //Act
-
-    //Assert
-    expect.fail('Remove this expect.fail and replace it with your test');
-
+    let expected = 2;
+    let actual = avgValue([1, 2, 3]);
+    expect(actual).to.equal(expected);
   })
 })
